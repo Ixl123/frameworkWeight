1aa4bc7cb81b56d01a1c4d135821fb6d59431a04
Ixl123 <cofman.jacob@gmx.de>
2016-09-29 11:38:50 +0200

    test

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/frameworkWeight/'
+    publicPath: 'frameworkWeight/dist/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
