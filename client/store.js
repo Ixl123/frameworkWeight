cda252a95898106e3104045527efb48f2513ffdc
Ixl123 <cofman.jacob@gmx.de>
2016-11-22 17:03:28 +0100

    removed localstorage feature

--- client/store.js
+++ client/store.js
@@ -1,5 +1,5 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-import { fetchLibrary } from './actions/actionCreators';
+import {createStore, compose, applyMiddleware} from 'redux';
+import {fetchLibrary} from './actions/actionCreators';
 import React from 'react';
 
 // import the root reducer
@@ -15,11 +15,9 @@ import filteredLibraries from './data/filteredLibraries';
 import thunkMiddleware from 'redux-thunk'
 
 import createLogger from 'redux-logger'
-import { loadState, saveState } from './localStorage';
-// accessibility 
-// var a11y = require('react-a11y');
-
-// import throttle to only run save state function only once a second
+// import { loadState, saveState } from './localStorage'; accessibility var a11y
+// = require('react-a11y'); import throttle to only run save state function
+// only once a second
 import throttle from 'lodash/throttle';
 
 const loggerMiddleware = createLogger()
@@ -32,32 +30,32 @@ const defaultState = {
 };
 
 let middleware = [thunkMiddleware];
-// check for  build or dev version. include loggermiddlerware only in dev 
+// check for  build or dev version. include loggermiddlerware only in dev
 if (process.env.NODE_ENV !== 'production') {
   // a11y(React);
-  middleware = [...middleware, loggerMiddleware];
+  middleware = [
+    ...middleware,
+    loggerMiddleware
+  ];
 } else {
   middleware = [...middleware];
 }
-const enhancers = compose(
-  applyMiddleware(...middleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-);
-const persistedState = loadState();
+const enhancers = compose(applyMiddleware(...middleware), window.devToolsExtension
+  ? window.devToolsExtension()
+  : f => f);
+//const persistedState = loadState();
 
-const store = createStore(rootReducer,
-  persistedState === undefined && process.env.NODE_ENV === 'production' ? defaultState : persistedState,
-  enhancers);
-// only write to localstorage once a second 
-store.subscribe(throttle(() => {
-  saveState(store.getState());
-}, 1000));
+const store = createStore(rootReducer, defaultState, enhancers);
+// only write to localstorage once a second store.subscribe(throttle(() => {
+// saveState(store.getState()); }, 1000));
 
 if (module.hot) {
-  module.hot.accept('./reducers/rootReducer', () => {
-    const nextRootReducer = require('./reducers/rootReducer').default;
-    store.replaceReducer(nextRootReducer);
-  });
+  module
+    .hot
+    .accept('./reducers/rootReducer', () => {
+      const nextRootReducer = require('./reducers/rootReducer').default;
+      store.replaceReducer(nextRootReducer);
+    });
 }
 
 export default store;
